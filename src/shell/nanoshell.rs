d346a9eb29268122a0e21b52d982d2bdddd20b75
Jkutkut <jorgeregonzalez@gmail.com>
2022-09-24 20:14:25 +0200

    Clear command implemented. Nothing command implemented. Ready to add handler

--- src/shell/nanoshell.rs
+++ src/shell/nanoshell.rs
@@ -15,7 +15,9 @@ impl Nanoshell<'_> {
         loop {
             input = self.get_input();
             match input.as_str() {
-                "exit\n" => break,
+                "exit" => break,
+                "clear" => self.clear_screen(),
+                "" => {},
                 _ => self.print("Command not found\n"),
             }
         }
@@ -24,18 +26,24 @@ impl Nanoshell<'_> {
     }
 
     fn get_input(&self) -> String {
-        // TODO remove \n
         let mut r = String::new();
         self.print(self.promt);
         io::stdin()
             .read_line(&mut r)
             .expect("Failed to read line");
+        r = r[..r.len() - 1].to_string(); // Remove \n
         r
     }
 
     fn print(&self, text: &str) {
-        //std::io::stdout().write(text);
         print!("{text}");
         io::stdout().flush().unwrap();
+    }
+
+    fn clear_screen(&self) {
+        let esc_char: char = 27 as char;
+        // 033[H -> Set cursor on home position.
+        // 033[2J -> Clear screen.
+        print!("{esc_char}[H{esc_char}[2J");
     }
 }
