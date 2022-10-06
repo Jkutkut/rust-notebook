cec616cfebd84f91e3a1c567e659311f3c5e7347
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-06 19:39:53 +0200

    now the db location is given by arguments to the program

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,21 @@ mod shell;
 
 mod notebook_sqlite;
 
+use std::env;
+
 #[async_std::main]
 async fn main() {
-    let mut nb: Notebook = Notebook::new("notebook.db").await;
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() != 2 {
+        if argv.len() < 2 {
+            print!("Enter the DB location as first parameter\n");
+        }
+        else {
+            print!("Too many parameters\n");
+        }
+        return;
+    }
+    let file: &str = argv[1].as_str();
+    let mut nb: Notebook = Notebook::new(file).await;
     nb.run();
 }
